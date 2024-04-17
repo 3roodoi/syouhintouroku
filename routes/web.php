79387854ac8abc7eff674cf3b2687d9cf39a2342
Takanobu Odoi <oodoi@3rrr-hd.jp>
2024-04-17 11:24:12 +0900

    テスト

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\FacadesR\oute;
+use app\Http\Controllers\TodoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('todos', [TodoController::class, 'index']);
+Route::get('todos/create', [TodoController::class, 'create']);
+Route::post('todos', [TodoController::class, 'store']);
+Route::get('todos/{id}', [TodoController::class, 'show']);
+Route::get('todos/{id}/edit', [TodoController::class, 'edit']);
+Route::put('todos/{id}', [TodoController::class, 'update']);
+Route::delete('todos/{id}', [TodoController::class, 'destroy']);
