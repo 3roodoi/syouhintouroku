4b7dd5af130fb719ffc34e794f67f1c3dc59d3e2
Takanobu Odoi <oodoi@3rrr-hd.jp>
2024-04-23 15:48:16 +0900

    ログイン画面の設定

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,78 +1,80 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '_', app()->getLocale()) }}">
-
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>商品登録</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 </head>
-
 <body>
-    <nav class="navbar navbar-light bg-light mb-3">
-        <a class="navber-brand" href="{{ url("http://localhost/todos") }}">商品登録</a>
-    </nav>
-    <div class="container-fluid">
-        @yield('content')
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
-    </script>
-
-{{-- ページネーション --}}
-<div style="margin: 1%;">
-    <table id="container" class="table table-sm table-bordered" style="width: 500px;"></table>
-    <nav class="pagination-container"><ul class="pagination" id="pagination"></ul></nav>
-</div>
-
-<script>
-    // ページ数を取得
-    var page = Number(getQueryParam('page'));
-    if (page < 1) page = 1;
-
-    count = 1015;
-    perPage = 20;
-    maxPage = Math.ceil(count / perPage) // 51
-
-    // ページネーション
-    html = '';
-    if (page > 1) {
-        html += `<li class="page-item"><a class="page-link" href="?page=${page - 1}">前へ</a></li>`;
-    }
-    for (i = page - 2; i <= page + 2 && i <= maxPage; i++) {
-        if (i < 1) continue;
-        if (i == page) {
-            html += `<li class="page-item active"><a class="page-link" href="?page=${i}">${i}</a></li>`;
-            continue;
-        }
-        html += `<li class="page-item"><a class="page-link" href="?page=${i}">${i}</a></li>`;
-    }
-    if (page != maxPage) {
-        html += `<li class="page-item"><a class="page-link" href="?page=${page + 1}">次へ</a></li>`
-    }
-    document.getElementById('pagination').innerHTML = html;
-
-    // URLから指定したパラメータを取得
-    function getQueryParam($key) {
-        if (1 < document.location.search.length) {
-            var query = document.location.search.substring(1);
-            var parameters = query.split('&');
-            for (var i = 0; i < parameters.length; i++) {
-                // パラメータ名とパラメータ値に分割する
-                var element = parameters[i].split('=');
-                if (element[0] == $key) return element[1];
-            }
-        }
-        return null;
-    }
-</script>
-    <style>
-        .pagination-container {
-        display: flex;
-        justify-content: center;
-        }
-    </style>
 </body>
+</html>
