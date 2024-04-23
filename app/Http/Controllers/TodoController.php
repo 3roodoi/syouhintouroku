8ca0640db1ada425e371246b2d52a7cc32ab2ad0
Takanobu Odoi <oodoi@3rrr-hd.jp>
2024-04-23 13:49:31 +0900

    ページネーション機能の追加

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -13,7 +13,8 @@ class TodoController extends Controller
      */
 public function index()
 {
-    $todos = Todo::all();
+    // $todos = Todo::all();
+    $todos = Todo::paginate(5);
 
     return view('todo.index', compact('todos'));
 }
